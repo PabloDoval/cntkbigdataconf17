28e26327e9536da6de8fe43fb8b654d14f9ada20
Rodrigo Cabello <mrcabello@plainconcepts.com>
2017-08-10 10:56:11 +0200

    Set default device GPU if the computer has one

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import cntk
 
 if __name__ == '__main__':
-    print(cntk.__version__)
-    print(cntk.device.all_devices())
+    print('CNTK version:',cntk.__version__)
+    all_devices = cntk.device.all_devices()
+    if all_devices[0].type() == cntk.device.DeviceKind.GPU:
+        print('You can use the GPU of your computer!!!')
+        cntk.device.try_set_default_device(cntk.device.gpu(0))
+    else:
+        print('Sorry, your computer only has a slow CPU')
+        cntk.device.try_set_default_device(cntk.device.cpu())
